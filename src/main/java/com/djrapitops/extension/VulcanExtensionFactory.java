48fb36a4d5fd52651fd7063794530653c3fe9f03
ahdg <ahdg6@outlook.com>
2021-07-02 23:39:36 +0800

    Implement Vulcan Violation Provider

--- src/main/java/com/djrapitops/extension/VulcanExtensionFactory.java
+++ src/main/java/com/djrapitops/extension/VulcanExtensionFactory.java
@@ -31,11 +31,11 @@ import java.util.Optional;
  *
  * @author AuroraLS3
  */
-public class NewExtensionFactory {
+public class VulcanExtensionFactory {
 
     private boolean isAvailable() {
         try {
-            Class.forName("");
+            Class.forName("me.frep.vulcan.api.VulcanAPI");
             return true;
         } catch (ClassNotFoundException e) {
             return false;
@@ -44,7 +44,7 @@ public class NewExtensionFactory {
 
     public Optional<DataExtension> createExtension() {
         if (isAvailable()) {
-            return Optional.of(new NewExtension());
+            return Optional.of(new VulcanExtension());
         }
         return Optional.empty();
     }
